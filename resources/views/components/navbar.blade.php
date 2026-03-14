79a3fdfbdd3ec72a3e6d666980be17d1471f7c1f
5unzet <spam091-cip@yahoo.co.id>
2026-03-14 22:30:57 +0700

    perbaikan. navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -4,7 +4,10 @@
             <img src="/assets/logo.png" alt="Logo" style="height: 40px; width: 40px; object-fit: contain; border-radius: 50%;">
             <span class="fw-bold">Bukit Eon</span>
         </a>
-        <div class="collapse navbar-collapse">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
                 <li class="nav-item"><a class="nav-link" href="/">Beranda</a></li>
                 <li class="nav-item"><a class="nav-link" href="/booking">Pesan Makanan</a></li>
